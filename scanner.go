b32150118b0aed1c8c2dc675e5e0f01e932f3f26
eliquious <max.franks@synapse-wireless.com>
2015-07-26 23:35:44 -0500

    Removed buffered scanner

--- scanner.go
+++ scanner.go
@@ -306,6 +306,7 @@ func (s *Scanner) scanDigits() string {
 	return buf.String()
 }
 
+// ScanRegex tokenizes a regex expression
 func (s *Scanner) ScanRegex() (tok Token, pos Pos, lit string) {
 	_, pos = s.r.curr()
 
@@ -323,57 +324,4 @@ func (s *Scanner) ScanRegex() (tok Token, pos Pos, lit string) {
 		return BADREGEX, pos, lit
 	}
 	return REGEX, pos, string(b)
-}
-
-// bufScanner represents a wrapper for scanner to add a buffer.
-// It provides a fixed-length circular buffer that can be unread.
-type bufScanner struct {
-	s   *Scanner
-	i   int // buffer index
-	n   int // buffer size
-	buf [3]struct {
-		tok Token
-		pos Pos
-		lit string
-	}
-}
-
-// newBufScanner returns a new buffered scanner for a reader.
-func newBufScanner(r io.Reader) *bufScanner {
-	return &bufScanner{s: NewScanner(r)}
-}
-
-// Scan reads the next token from the scanner.
-func (s *bufScanner) Scan() (tok Token, pos Pos, lit string) {
-	return s.scanFunc(s.s.Scan)
-}
-
-// ScanRegex reads a regex token from the scanner.
-func (s *bufScanner) ScanRegex() (tok Token, pos Pos, lit string) {
-	return s.scanFunc(s.s.ScanRegex)
-}
-
-// scanFunc uses the provided function to scan the next token.
-func (s *bufScanner) scanFunc(scan func() (Token, Pos, string)) (tok Token, pos Pos, lit string) {
-	// If we have unread tokens then read them off the buffer first.
-	if s.n > 0 {
-		s.n--
-		return s.curr()
-	}
-
-	// Move buffer position forward and save the token.
-	s.i = (s.i + 1) % len(s.buf)
-	buf := &s.buf[s.i]
-	buf.tok, buf.pos, buf.lit = scan()
-
-	return s.curr()
-}
-
-// Unscan pushes the previously token back onto the buffer.
-func (s *bufScanner) Unscan() { s.n++ }
-
-// curr returns the last read token.
-func (s *bufScanner) curr() (tok Token, pos Pos, lit string) {
-	buf := &s.buf[(s.i-s.n+len(s.buf))%len(s.buf)]
-	return buf.tok, buf.pos, buf.lit
 }
